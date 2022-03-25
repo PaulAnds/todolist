38280618dbfdc4957d5e961d91577f72dd349f97
unknown <paul.solis@uniat.edu.mx>
2022-03-25 13:26:18 -0600

    Todo start

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import Header from '../shared/layout/Header.js';
 import Footer from '../shared/layout/Footer.js';
 import Content from '../shared/layout/Content.js';
 import './App.css';
+import Todo from './Todo/Todo.js';
 
 function App() {
   return (
     <div className="App">
       <Header title="Header" url="https://www.youtube.com/watch?v=mCdA4bJAGGk" />
       <Content>
-        <Home />
+        <Todo />
       </Content>
       <Footer />
     </div>
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {v4 as uuidv4} from 'uuid';
 
 export default class Todo extends Component {
 
@@ -6,11 +7,33 @@ export default class Todo extends Component {
         super();
         
         this.state = {
-            
+            task: '',
+            items: [
+              {
+                  id: uuidv4(),
+                  task: 'Estudiar Web',
+                  complete: false,
+              },
+              
+              {
+                  id: uuidv4(),
+                  task: 'Hacer Tarea',
+                  complete: false,
+              },
+
+              {
+                  id: uuidv4(),
+                  task: 'Ver One Piece',
+                  complete: false,
+              },
+            ],
         }
     }
 
     render() {
+
+      console.table(this.state.items)
+
         return (
             <div className="Todo">
               <h1>Nueva Tarea</h1>
