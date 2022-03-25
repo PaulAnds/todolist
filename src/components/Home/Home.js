7de0d38ba4526bb941a0a86a37b7a377c41f44b9
unknown <paul.solis@uniat.edu.mx>
2022-03-25 10:37:03 -0600

    buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
     render() {
+
+        const buttonStyle = {
+            backgroundColor: 'red',
+            border: '1px solid black',
+        }
+
         return (
-            <div>
-                
+            <div className="Home">
+                <h1>Home</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum arcu, at tempus erat. Duis ac pharetra tellus. Quisque a mi a massa sodales consequat. Donec dapibus euismod accumsan. Duis blandit cursus lacus, eget dapibus eros. Sed augue nibh, fringilla vel varius vitae, porta quis arcu. Aenean dolor erat, laoreet sit amet auctor sed, auctor sit amet ligula. Phasellus vel augue fringilla, ultrices felis at, ornare nisi. Proin porttitor diam eget rhoncus vulputate. In molestie diam quis urna pellentesque tempus. Integer ullamcorper magna dolor, eget maximus diam pulvinar eget. Suspendisse potenti.</p>
+                <a href="https://www.youtube.com/watch?v=mCdA4bJAGGk">Find a girlfriend</a>
+
+                <div>
+                    {{/* Styles inline */}}
+                    <button style= {{
+                        backgroundColor: 'MediumSeaGreen',
+                        border: '1px solid black',
+                    }}>
+                        click
+                    </button>
+                    {{/* Styles objects */}}
+                    <button style ={buttonStyle}>
+                        click
+                    </button>
+                </div>
             </div>
         );
     }
